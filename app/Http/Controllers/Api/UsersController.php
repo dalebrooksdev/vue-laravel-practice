6d44f50cb4ad2bca29d29119bb1954eeececd1cd
Andrew Brooks <hellodalebrooks@gmail.com>
2019-11-17 22:07:56 -0600

    Initial commit

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use App\Http\Resources\UserResource;
+class UsersController extends Controller
+{
+    public function index(){
+        return UserResource::collection(User::paginate(10));
+    }
+}
